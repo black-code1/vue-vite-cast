533a3b333299ff15f5ef1cf37e7446ce23f16710
black-code1 <legrandmahamat1@gmail.com>
2022-07-21 11:09:13 +0100

    Two Mental Leaps to Script Setup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,25 @@
 <script setup>
 import TheWelcome from "@/components/TheWelcome.vue";
+
+// eslint-disable-next-line no-undef
+let message = $ref("Hello, World!");
+
+let doSomething = () => {
+  alert("doing it now")
+}
+
 </script>
 
 <template>
   <main>
     <TheWelcome />
+
+    <p>{{message}}</p>
+    <p>
+      <input type="text" v-model="message" />
+    </p>
+    <p>
+      <button @click="doSomething">Click Me</button>
+    </p>
   </main>
 </template>
