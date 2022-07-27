b2c2a400397f6d19540117ef31295037bd83e368
black-code1 <legrandmahamat1@gmail.com>
2022-07-27 11:45:06 +0100

    Two Ways to Transition

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -5,19 +5,31 @@ defineProps({
 </script>
 
 <template>
-  <div v-if="show" class="modal-mask">
-    <div class="modal-container">
-      <div>
-        <slot>default body</slot>
-      </div>
+<!--  <Transition name="modal">-->
 
-      <footer class="modal-footer">
-        <slot name="footer">
-          <button @click="$emit('close')">Close</button>
-        </slot>
-      </footer>
+<!--  </Transition>-->
+  <Transition
+    enter-from-class="opacity-0 scale-125"
+    enter-to-class="opacity-100 scale-100"
+    enter-active-class="transition duration-300"
+    leave-active-class="transition duration-200"
+    leave-from-class="opacity-100 scale-100"
+    leave-to-class="opacity-0 scale-125"
+  >
+    <div v-if="show" class="modal-mask">
+      <div class="modal-container">
+        <div>
+          <slot>default body</slot>
+        </div>
+
+        <footer class="modal-footer">
+          <slot name="footer">
+            <button @click="$emit('close')">Close</button>
+          </slot>
+        </footer>
+      </div>
     </div>
-  </div>
+  </Transition>
 </template>
 
 
@@ -53,5 +65,17 @@ defineProps({
 
 .modal-footer button:hover{
   background: #c8c8c8;
+}
+
+.modal-enter-active, .modal-leave-active{
+  transition: opacity 1s;
+}
+
+.modal-enter-from, .modal-leave-to{
+  opacity: 0;
+}
+
+.modal-enter-to, .modal-leave-from{
+  opacity: 100;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,7 @@ import { createApp } from "vue";
 import {createPinia} from "pinia"
 import App from "./App.vue";
 import router from "./router";
-
-import "./assets/index.css";
+import './index.css'
 
 const app = createApp(App);
 
